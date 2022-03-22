0ea5fbf9932e9fca9bb65ffbaf5fae7089adc12a
Matías Gabler <mngabler@uc.cl>
2022-03-22 20:37:05 -0300

    🚧 Fish

---
+++ src/components/TeamMembers/TeamMembers.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TeamMembers() {
+
+    return (
+      <div className="team-members">
+        <img src={'fish-resume.svg'} className="member"/>
+      </div>
+    );
+  }
--- src/components/WaterContainer/WaterContainer.jsx
+++ src/components/WaterContainer/WaterContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./WaterContainer.scss";
-import LogoContainer from '../Logo Container/LogoContainer';
+import TeamMembers from "../TeamMembers/TeamMembers";
 
 export default function WaterContainer() {
 
@@ -10,6 +10,7 @@ export default function WaterContainer() {
             <img src={'wave_tops.svg'}/>
         </div>
         <div className="under-water">
+          <TeamMembers></TeamMembers>
         </div>
       </div>
     );
