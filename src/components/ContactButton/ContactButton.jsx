5716a3c2a388e697a567a28f5f86c6e304760795
Matías Gabler <mngabler@uc.cl>
2022-03-23 17:55:26 -0300

    📜 Lets Talk Mobile

--- src/components/ContactButton/ContactButton.jsx
+++ src/components/ContactButton/ContactButton.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { isDesktop } from "react-device-detect";
 import "./ContactButton.scss";
 
 export default function ContactButton() {
 
-    return (
-      <div className="contact-button-container">
-        <div className="contact-button">
-          <img src={'mail.svg'} className="mail-icon"/>
-          <div className="text">LET'S TALK!</div>
+    if (isDesktop)
+    {
+      // Desktop.
+      return (
+        <div className="contact-button-container">
+          <div className="contact-button">
+            <img src={'mail.svg'} className="mail-icon"/>
+            <div className="text">LET'S TALK!</div>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      // Mobile.
+      return (
+        <div className="contact-button-container">
+          <div className="contact-button-mobile">
+            <div className="text">LET'S TALK!</div>
+          </div>
+        </div>
+      );
+    }
   }
