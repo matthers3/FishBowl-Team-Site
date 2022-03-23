5444faf2576386ce770106e1d24b8ebf02e4bfd1
Matías Gabler <mngabler@uc.cl>
2022-03-23 18:07:55 -0300

    📜 Slider

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserView } from "react-device-detect";
+import { isDesktop } from "react-device-detect";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
 
@@ -11,9 +11,9 @@ import 'swiper/css/autoplay';
 import "./Projects.scss";
 
 export default function Projects() {
-    return (
-        <div>
-            <BrowserView className="desktop-projects">
+    if (isDesktop) {
+        return (
+            <div className="desktop-projects">
                 <div className="project-title text-big">
                     <div className="orange-text">Alma</div>
                     <div className="right-text"> is NOW in development !</div>
@@ -63,7 +63,61 @@ export default function Projects() {
                     <div className="soon-text">Coming soon !</div>
                     <div className="pun-text">(you know, like a liar)</div>
                 </div>
-            </BrowserView>
-        </div>
-    );
+            </div>
+        );
+    } else {
+        return (
+            <div className="mobile-projects">
+                <div className="project-title mobile-text-big">
+                    <div className="orange-text">Alma</div>
+                    <div className="right-text"> is NOW in development !</div>
+                </div>
+                <div className="image-swiper">
+                    <Swiper
+                        // install Swiper modules
+                        modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+                        spaceBetween={0}
+                        slidesPerView={1}
+                        autoplay={{
+                            delay: 2000,
+                        }}
+                        loop={true}
+                        loopedSlides={5}
+                        pagination={{ clickable: true }}
+                        scrollbar={{ draggable: true }}
+                        >
+                        <SwiperSlide>
+                            <div className="image-cut">
+                                <img src="swiper1.JPG"/>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="image-cut">
+                                <img src="swiper2.JPG"/>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="image-cut">
+                                <img src="swiper3.JPG"/>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="image-cut">
+                                <img src="swiper4.JPG"/>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="image-cut">
+                                <img src="swiper5.JPG"/>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+                <div className="project-close">
+                    <div className="soon-text">Coming soon !</div>
+                    <div className="pun-text">(you know, like a liar)</div>
+                </div>
+            </div>
+        );
+    }
 }
