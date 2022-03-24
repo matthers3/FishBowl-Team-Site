a9d382ecbecab5cfc51d46f398bc576cc2560521
Matías Gabler <mngabler@uc.cl>
2022-03-24 09:08:10 -0300

    📜 Meet The Team Parametrics

--- src/components/TeamMembers/TeamMembers.jsx
+++ src/components/TeamMembers/TeamMembers.jsx
@@ -1,34 +1,107 @@
 import React from "react";
+import { isDesktop } from "react-device-detect";
 import "./TeamMembers.scss";
 
 export default function TeamMembers() {
 
-    return (
-      <div className="team-members">
-        <div className="team-title text-big">
-          Meet the Team
+    const members = [
+      {
+        name: '',
+        role: '',
+        description: ''
+      },
+      {
+        name: '',
+        role: '',
+        description: ''
+      },
+      {
+        name: '',
+        role: '',
+        description: ''
+      },
+      {
+        name: '',
+        role: '',
+        description: ''
+      },
+    ]
+
+    const meetTheTeam = {
+      title: 'Meet the team',
+      p1: {
+        l1: 'We are a small, independent team from Santiago, Chile.',
+        l2: 'Joined by our love for worldbuilding, interactive storytelling, beautiful visual',
+        l3: 'experiences and always looking to achieve fun first! We make games that we',
+        l4: 'wanna play and build worlds that have something to say.'
+      },
+      p2: {
+        l1: 'We like meaningful stories and we’d like to make stories that are meaningful',
+        l2: 'to you too. '
+      }
+    }
+
+    if (isDesktop)
+    {
+      return (
+        <div className="team-members">
+          <div className="team-title text-big">
+            {meetTheTeam.title}
+          </div>
+          <div className="about-text">
+            <div className="p1">
+            {meetTheTeam.p1.l1}<br/>
+            {meetTheTeam.p1.l2}<br/>
+            {meetTheTeam.p1.l3}<br/>
+            {meetTheTeam.p1.l4}<br/>
+            </div>
+            <div className="p2">
+            {meetTheTeam.p2.l1}<br/>
+            {meetTheTeam.p2.l2}
+            </div>
+          </div>
+          <div className="the-team">
+            <div className="row">
+              <img src={'Fishfacingright_01.svg'} className="member"/>
+              <img src={'Fishfacingright_01.svg'} className="member member-low"/>
+            </div>
+            <div className="row">
+              <img src={'Fishfacingleft_01.svg'} className="member"/>
+              <img src={'Fishfacingleft_01.svg'} className="member member-low"/>
+            </div>
+          </div>
         </div>
-        <div className="about-text">
-          <div className="p1">
-          We are a small, independent team from Santiago, Chile.<br/>
-          Joined by our love for worldbuilding, interactive storytelling, beautiful visual <br/>
-          experiences and always looking to achieve fun first! We make games that we<br/>
-          wanna play and build worlds that have something to say.
+      );
+    } else {
+      // Mobile.
+      return (
+        <div className="team-members">
+          <div className="team-title text-big">
+            {meetTheTeam.title}
           </div>
-          <div className="p2">
-          We like meaningful stories and we’d like to make stories that are meaningful<br/>to you too. 
+          <div className="about-text">
+            <div className="p1">
+            {meetTheTeam.p1.l1}<br/>
+            {meetTheTeam.p1.l2}<br/>
+            {meetTheTeam.p1.l3}<br/>
+            {meetTheTeam.p1.l4}<br/>
+            </div>
+            <div className="p2">
+            {meetTheTeam.p2.l1}<br/>
+            {meetTheTeam.p2.l2}
+            </div>
+          </div>
+          <div className="the-team">
+            <div className="row">
+              <img src={'Fishfacingright_01.svg'} className="member"/>
+              <img src={'Fishfacingright_01.svg'} className="member member-low"/>
+            </div>
+            <div className="row">
+              <img src={'Fishfacingleft_01.svg'} className="member"/>
+              <img src={'Fishfacingleft_01.svg'} className="member member-low"/>
+            </div>
           </div>
         </div>
-        <div className="the-team">
-          <div className="row">
-            <img src={'Fishfacingright_01.svg'} className="member"/>
-            <img src={'Fishfacingright_01.svg'} className="member member-low"/>
-          </div>
-          <div className="row">
-            <img src={'Fishfacingleft_01.svg'} className="member"/>
-            <img src={'Fishfacingleft_01.svg'} className="member member-low"/>
-          </div>
-        </div>
-      </div>
-    );
+      );
+    }
   }
