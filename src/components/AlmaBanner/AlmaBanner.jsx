1d20b97584eaf855fd5e2bb23dc1ce8bab6511b6
MatiasGabler <mngabler@uc.cl>
2024-03-14 17:03:11 -0300

    ♻️ Fix Banner

--- src/components/AlmaBanner/AlmaBanner.jsx
+++ src/components/AlmaBanner/AlmaBanner.jsx
@@ -9,7 +9,7 @@ export default function AlmaBanner() {
       //Desktop.
       return (
         <div className="alma-banner" id="home">
-            <div className="bannerContainer" style={{backgroundImage: `url(./FC_Alma_Cover_Art_NOLOGO.png)`}}>
+            <div className="bannerContainer" style={{backgroundImage: `url(./FCR_banner.png)`}}>
                 <img className="imageSizer" alt="" src={"./AlmaBannerSizer.png"}></img>
             </div>
             {/* <img className="alma-title" alt="" src={"./Logo_WIP.png"} /> */}
@@ -20,8 +20,8 @@ export default function AlmaBanner() {
       return (
         // Mobile.
         <div className="mobile-alma-banner" id="home">
-            <div className="bannerContainer" style={{backgroundImage: `url(./FC_Alma_Cover_Art_NOLOGO.png)`}}>
-                <img className="imageSizer" alt="" src={"./AlmaBannerSizer.png"}></img>
+            <div className="bannerContainer" style={{backgroundImage: `url(./FCR_banner.png)`}}>
+                <img className="imageSizer" alt="" src={"./FCR_banner.png"}></img>
             </div>
             {/* <img className="alma-title" alt="" src={"./Logo_WIP.png"} /> */}
         </div>
